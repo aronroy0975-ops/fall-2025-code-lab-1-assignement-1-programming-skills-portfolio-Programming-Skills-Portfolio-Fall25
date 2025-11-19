99c3105b711c21e1c45678691bf6be01c1b0c8b4
Aron <aronroy0975@gmail.com>
2025-11-19 23:31:14 +0400

    Simple Sums

---
+++ Exercise 02- Simple Sums/Qns2.py
@@ -0,0 +1,11 @@
+# keeping the first number
+first_number = 8
+
+# keeping the second number
+second_number = 10
+
+# adding the two numbers
+sum_of_numbers = first_number + second_number
+
+# showing the final sum
+print(sum_of_numbers)
